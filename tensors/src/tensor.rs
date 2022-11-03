e447f51957e6fb3c5897b88b6d0e1132ffe91185
neilcantorne <cantorne.neil@gmail.com>
2022-11-04 07:12:08 +0800

    impl Display and Debug

--- tensors/src/tensor.rs
+++ tensors/src/tensor.rs
@@ -1,6 +1,8 @@
+use std::fmt::{Debug, Display, Write};
+
 #[derive(Clone, Copy)]
 pub struct Tensor<T, const ROW: usize, const COLUMN: usize> {
-    elements: [[T; ROW]; COLUMN]
+    elements: [[T; COLUMN]; ROW]
 }
 
 impl<T, const ROW: usize, const COLUMN: usize> Tensor<T, ROW, COLUMN> 
@@ -8,8 +10,40 @@ impl<T, const ROW: usize, const COLUMN: usize> Tensor<T, ROW, COLUMN>
 
     pub fn filled_with(value: T) -> Self {
         Self {
-            elements: [[value; ROW]; COLUMN]
+            elements: [[value; COLUMN]; ROW]
         }
+    }
+}
+
+impl<T, const ROW: usize, const COLUMN: usize> Display for Tensor<T, ROW, COLUMN>
+    where T: Display {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut r = 0usize;
+        let mut c = 0usize;
+
+        f.write_char('[')?;
+
+        loop {
+            self.elements[r][c].fmt(f)?;
+            
+            c += 1;
+
+            if c < COLUMN {
+                f.write_str(", ")?;
+            }
+            else if r < ROW - 1 {
+                r += 1; c = 0;
+                f.write_str(" | ")?;
+            }
+            else { return f.write_char(']'); }
+        }
+    }
+}
+
+impl<T, const ROW: usize, const COLUMN: usize> Debug for Tensor<T, ROW, COLUMN> 
+    where T: Display {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        <Self as Display>::fmt(&self, f)
     }
 }
 
@@ -17,15 +51,15 @@ impl<T, const ROW: usize, const COLUMN: usize> Default for Tensor<T, ROW, COLUMN
     where T: Default + Copy + Sized {
     fn default() -> Self {
         Self { 
-            elements: [[T::default(); ROW]; COLUMN]
+            elements: [[T::default(); COLUMN]; ROW]
         }
     }
 }
 
-impl<T, const ROW: usize, const COLUMN: usize> From<[[T; ROW]; COLUMN]> for Tensor<T, ROW, COLUMN>
+impl<T, const ROW: usize, const COLUMN: usize> From<[[T; COLUMN]; ROW]> for Tensor<T, ROW, COLUMN>
     where T: Copy {
     
-    fn from(array: [[T; ROW]; COLUMN]) -> Self {
+    fn from(array: [[T; COLUMN]; ROW]) -> Self {
         Self { elements: array }
     }
 }
