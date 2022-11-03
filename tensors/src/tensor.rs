35a3a2d7e1ad0b52e5df7a0290bb352507eedbe6
neilcantorne <cantorne.neil@gmail.com>
2022-11-04 00:22:26 +0800

    impl filled_with

--- tensors/src/tensor.rs
+++ tensors/src/tensor.rs
@@ -3,6 +3,16 @@ pub struct Tensor<T, const ROW: usize, const COLUMN: usize> {
     elements: [[T; ROW]; COLUMN]
 }
 
+impl<T, const ROW: usize, const COLUMN: usize> Tensor<T, ROW, COLUMN> 
+    where T: Copy {
+
+    pub fn filled_with(value: T) -> Self {
+        Self {
+            elements: [[value; ROW]; COLUMN]
+        }
+    }
+}
+
 impl<T, const ROW: usize, const COLUMN: usize> Default for Tensor<T, ROW, COLUMN>
     where T: Default + Copy + Sized {
     fn default() -> Self {
