90b40ee9a92c2ab09676d504973231a7d46a5e1d
neilcantorne <cantorne.neil@gmail.com>
2022-11-04 00:09:50 +0800

    impl Default for Tensor

--- tensors/src/tensor.rs
+++ tensors/src/tensor.rs
@@ -2,3 +2,12 @@
 pub struct Tensor<T, const ROW: usize, const COLUMN: usize> {
     elements: [[T; ROW]; COLUMN]
 }
+
+impl<T, const ROW: usize, const COLUMN: usize> Default for Tensor<T, ROW, COLUMN>
+    where T: Default + Copy + Sized {
+    fn default() -> Self {
+        Self { 
+            elements: [[T::default(); ROW]; COLUMN]
+        }
+    }
+}
