c41c25e710727fbc4b11de76d3773aeab67f27a3
neilcantorne <cantorne.neil@gmail.com>
2022-11-05 06:22:33 +0800

    implement Iterators

--- tensors/src/tensor.rs
+++ tensors/src/tensor.rs
@@ -13,6 +13,22 @@ impl<T, const ROW: usize, const COLUMN: usize> Tensor<T, ROW, COLUMN>
             elements: [[value; COLUMN]; ROW]
         }
     }
+
+    pub fn hr_iter<'a>(&'a self) -> HorizontalIter<'a, T, ROW, COLUMN> {
+        HorizontalIter::<'a, T, ROW, COLUMN> {
+            tensor: self,
+            index_r: 0,
+            index_c: 0,
+        }
+    }
+
+    pub fn vr_iter<'a>(&'a self) -> VerticalIter<'a, T, ROW, COLUMN> {
+        VerticalIter::<'a, T, ROW, COLUMN> {
+            tensor: self,
+            index_r: 0,
+            index_c: 0,
+        }
+    }
 }
 
 impl<T, const ROW: usize, const COLUMN: usize> Display for Tensor<T, ROW, COLUMN>
@@ -77,5 +93,66 @@ impl<T, const ROW: usize, const COLUMN: usize> From<[[T; COLUMN]; ROW]> for Tens
     
     fn from(array: [[T; COLUMN]; ROW]) -> Self {
         Self { elements: array }
+    }
+}
+
+// Elements Iterator
+pub struct HorizontalIter<'a, T, const ROW: usize, const COLUMN: usize> {
+    pub(self) tensor: &'a Tensor<T, ROW, COLUMN>,
+    pub(self) index_r: usize,
+    pub(self) index_c: usize,
+}
+
+impl<'a, T, const ROW: usize, const COLUMN: usize> Iterator for HorizontalIter<'a, T, ROW, COLUMN> {
+    type Item = (&'a T, usize, usize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+
+        if self.index_c >= COLUMN {
+            self.index_c = 0;
+            self.index_r += 1;
+            
+            if self.index_r >= ROW { return None; }
+        }
+
+        let next =  Some((
+            &self.tensor.elements[self.index_r][self.index_c],
+            self.index_r,
+            self.index_c
+        ));
+
+        self.index_c += 1;
+        
+        return next;
+    }
+}
+
+pub struct VerticalIter<'a, T, const ROW: usize, const COLUMN: usize> {
+    pub(self) tensor: &'a Tensor<T, ROW, COLUMN>,
+    pub(self) index_r: usize,
+    pub(self) index_c: usize,
+}
+
+impl<'a, T, const ROW: usize, const COLUMN: usize> Iterator for VerticalIter<'a, T, ROW, COLUMN> {
+    type Item = (&'a T, usize, usize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+
+        if self.index_r >= ROW {
+            self.index_r = 0;
+            self.index_c += 1;
+            
+            if self.index_c >= COLUMN { return None; }
+        }
+
+        let next =  Some((
+            &self.tensor.elements[self.index_r][self.index_c],
+            self.index_r,
+            self.index_c
+        ));
+
+        self.index_r += 1;
+        
+        return next;
     }
 }
