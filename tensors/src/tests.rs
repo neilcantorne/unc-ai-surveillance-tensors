dbe4ea8217f899f3743a63f4d66ebe629eb65336
neilcantorne <cantorne.neil@gmail.com>
2022-11-04 11:44:40 +0800

    integrate tensor macro in test

--- tensors/src/tests.rs
+++ tensors/src/tests.rs
@@ -1,26 +1,24 @@
+use crate::Tensor;
+use crate::tensor;
+
 #[test]
 fn test_tensor_display() {
-    let tensor = super::Tensor::<f32, 4, 4>::from([
-        [1.0f32, 0.0f32, 0.0f32, 0.0f32],
-        [0.0f32, 1.0f32, 0.0f32, 0.0f32],
-        [0.0f32, 0.0f32, 1.0f32, 0.0f32],
-        [0.0f32, 0.0f32, 0.0f32, 1.0f32],
-    ]);
+    let matrix = tensor!(
+        1.0f64, 0.0f64, 0.0f64, 0.0f64 |
+        0.0f64, 1.0f64, 0.0f64, 0.0f64 |
+        0.0f64, 0.0f64, 1.0f64, 0.0f64 |
+        0.0f64, 0.0f64, 0.0f64, 1.0f64);
 
-    assert!(format!("{:.2}", tensor) == "[1.00, 0.00, 0.00, 0.00 | 0.00, 1.00, 0.00, 0.00 | 0.00, 0.00, 1.00, 0.00 | 0.00, 0.00, 0.00, 1.00]",
+    assert!(format!("{:.2}", matrix) == "[1.00, 0.00, 0.00, 0.00 | 0.00, 1.00, 0.00, 0.00 | 0.00, 0.00, 1.00, 0.00 | 0.00, 0.00, 0.00, 1.00]",
         "Erroneous formatting of 4x4 Tensor display");
     
-    let tensor = super::Tensor::<f32, 1, 4>::from([
-        [1.0f32, 0.0f32, 0.1f32, 0.0f32],
-    ]);
+    let rvector = tensor![1.0f64, 0.0f64, 0.1f64, 0.0f64];
 
-    assert!(format!("{:.3}", tensor) == "[1.000, 0.000, 0.100, 0.000]",
+    assert!(format!("{:.3}", rvector) == "[1.000, 0.000, 0.100, 0.000]",
         "Erroneous formatting of 1x4 Tensor display");
     
-    let tensor = super::Tensor::<f32, 4, 1>::from([
-        [1.0f32], [2.0f32], [0.1f32], [0.0f32],
-    ]);
+    let cvector = tensor![1.0f64 | 2.0f64 | 0.1f64 | 0.0f64];
 
-    assert!(format!("{:.3}", tensor) == "[1.000 | 2.000 | 0.100 | 0.000]",
+    assert!(format!("{:.3}", cvector) == "[1.000 | 2.000 | 0.100 | 0.000]",
         "Erroneous formatting of 4x1 Tensor display");
 }
