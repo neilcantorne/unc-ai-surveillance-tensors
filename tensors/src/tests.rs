ea49db6bd3a3f60fe9fc512a629b229c8ee9b50d
neilcantorne <cantorne.neil@gmail.com>
2022-11-04 13:16:12 +0800

    test Tensor subscript operator

--- tensors/src/tests.rs
+++ tensors/src/tests.rs
@@ -2,14 +2,17 @@ use crate::Tensor;
 use crate::tensor;
 
 #[test]
-fn test_tensor_display() {
-    let matrix = tensor!(
+fn test_tensor() {
+    let matrix : Tensor<f64, 4, 4> = tensor!(
         1.0f64, 0.0f64, 0.0f64, 0.0f64 |
         0.0f64, 1.0f64, 0.0f64, 0.0f64 |
-        0.0f64, 0.0f64, 1.0f64, 0.0f64 |
+        0.0f64, 2.0f64, 1.0f64, 0.0f64 |
         0.0f64, 0.0f64, 0.0f64, 1.0f64);
 
-    assert!(format!("{:.2}", matrix) == "[1.00, 0.00, 0.00, 0.00 | 0.00, 1.00, 0.00, 0.00 | 0.00, 0.00, 1.00, 0.00 | 0.00, 0.00, 0.00, 1.00]",
+    assert!(matrix[(1, 2)] == 2.0,
+        "Erroneous Tensor index operation");
+
+    assert!(format!("{:.2}", matrix) == "[1.00, 0.00, 0.00, 0.00 | 0.00, 1.00, 0.00, 0.00 | 0.00, 2.00, 1.00, 0.00 | 0.00, 0.00, 0.00, 1.00]",
         "Erroneous formatting of 4x4 Tensor display");
     
     let rvector = tensor![1.0f64, 0.0f64, 0.1f64, 0.0f64];
