0e44d2516622b771b8cd18139fe851f5a59c8006
neilcantorne <cantorne.neil@gmail.com>
2022-11-09 09:20:01 +0800

    replace with struct Function implementation

--- macros/src/dl_bindings.rs
+++ macros/src/dl_bindings.rs
@@ -1,27 +1,42 @@
+use proc_macro2::Ident;
+use syn::ForeignItemFn;
 
+#[derive(Clone)]
 pub struct DlBinding {
-    pub abi: syn::LitStr,
-    pub functions: Vec<syn::ForeignItemFn>,
+    functions: Vec<Function>,
 }
 
 impl syn::parse::Parse for DlBinding {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         let externs = input.parse::<syn::ItemForeignMod>()?;
 
-        let abi = externs.clone()
-            .abi
-            .name
-            .ok_or(syn::Error::new_spanned(externs.abi, "Please specify an ABI"))?;
-
         let mut functions = Vec::new();
 
         for item in &externs.items {
             match item {
-                syn::ForeignItem::Fn(fn_item) => functions.push(fn_item.clone()),
+                syn::ForeignItem::Fn(fn_item) 
+                    => functions.push(Function::from(fn_item.clone())),
                 _ => continue,
             }
         }
 
-        Ok(Self { abi, functions })
+        Ok(Self { functions })
+    }
+}
+
+#[derive(Clone)]
+struct Function {
+    base: ForeignItemFn,
+    fnptr: Ident,
+}
+
+impl From<ForeignItemFn> for Function {
+    fn from(item: ForeignItemFn) -> Self {
+        Self {
+            fnptr: Ident::new(
+                format!("fn_{}", item.sig.ident.to_string()).as_str(),
+                item.sig.ident.span()),
+            base: item
+        }
     }
 }
