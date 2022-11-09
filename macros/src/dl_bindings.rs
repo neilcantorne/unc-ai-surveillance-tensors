f70cbf29653a4bb783ad7a2fd57fb6e466129ddd
neilcantorne <cantorne.neil@gmail.com>
2022-11-09 15:21:07 +0800

    impl fn caller for Function

--- macros/src/dl_bindings.rs
+++ macros/src/dl_bindings.rs
@@ -1,5 +1,23 @@
 use proc_macro2::Ident;
-use syn::ForeignItemFn;
+use syn::punctuated::Punctuated;
+use syn::Token;
+use syn::token::{SelfValue, And, Brace, Paren, Dot, Unsafe};
+use syn::{
+    FnArg,
+    Receiver,
+    Block,
+    Stmt,
+    Expr,
+    ExprCall,
+    ForeignItemFn,
+    ExprField,
+    Member,
+    ItemFn,
+    Path,
+    PathSegment,
+    Pat,
+    ExprParen,
+};
 
 #[derive(Clone)]
 pub struct DlBinding {
@@ -37,6 +55,89 @@ impl From<ForeignItemFn> for Function {
                 format!("fn_{}", item.sig.ident.to_string()).as_str(),
                 item.sig.ident.span()),
             base: item
+        }
+    }
+}
+
+macro_rules! ident_expr {
+    ($identifier:expr) => {
+        {
+            let mut segments = Punctuated::<PathSegment, Token![::]>::new();
+            
+            segments.push(PathSegment {
+                ident: {$identifier},
+                arguments: syn::PathArguments::None,
+            });
+
+            Expr::Path(syn::ExprPath {
+                attrs: vec![],
+                qself: None,
+                path: Path { leading_colon: None, segments  }
+            })
+        }
+    };
+}
+
+impl Function {
+    pub fn caller(&self) -> ItemFn {
+        let mut sig = self.base.sig.clone();
+        let span = sig.ident.span();
+
+        sig.abi = None;
+        sig.unsafety = Some(Unsafe { span });
+
+
+        let args = Punctuated::<Expr, Token![,]>::from_iter(sig.inputs.iter().filter_map(|param| {
+            if let FnArg::Typed(param) = param {
+                if let Pat::Ident(ident) = param.pat.as_ref() {
+                    return Some(ident_expr!(ident.ident.clone()))
+                }
+            }
+            None
+        }));
+
+        sig.inputs.insert(0, FnArg::Receiver(
+            Receiver {
+                attrs: Vec::new(),
+                reference: Some((And { spans: [span] }, None)),
+                mutability: None,
+                self_token: SelfValue { span },
+            }
+        ));
+
+        let statement = Stmt::Expr(
+            Expr::Call(
+            ExprCall {
+                attrs: vec![],
+                func: Box::new(
+                    Expr::Paren(ExprParen {
+                        attrs: vec![],
+                        paren_token: Paren { span },
+                        expr: Box::new(Expr::Field(
+                            ExprField {
+                                attrs: vec![],
+                                base: Box::new(ident_expr!(Ident::new("self", span))),
+                                dot_token: Dot { spans: [span] },
+                                member: Member::Named(self.fnptr.clone()),
+                            }
+                        ))
+                    }
+                )),
+                paren_token: Paren { span },
+                args,
+            }
+        ));
+
+        let block =  Box::new(Block {
+            brace_token: Brace::default(),
+            stmts: vec![ statement ],
+        });
+
+        syn::ItemFn {
+            attrs: self.base.attrs.clone(),
+            vis: self.base.vis.clone(),
+            sig,
+            block
         }
     }
 }
