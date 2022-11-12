0e436f1e495cbb41c1d23f29d70b92f5d03bf56f
neilcantorne <cantorne.neil@gmail.com>
2022-11-12 23:04:35 +0800

    edit imports

--- tensors/src/accelerator/mod.rs
+++ tensors/src/accelerator/mod.rs
@@ -1,7 +1,10 @@
 // Modules
 mod device;
+mod factory;
 mod backend;
 pub(crate) mod reflection;
 
 // Exported structs
 pub use device::Device;
+pub use factory::Factory;
+pub(crate) use backend::BackendError;
