2f9868e538ac79a11441ec2885d4f94d7455f428
neilcantorne <cantorne.neil@gmail.com>
2022-11-15 21:00:50 +0800

    impl fn size and fix layout size calculation

--- tensors/src/accelerator/array_buffer.rs
+++ tensors/src/accelerator/array_buffer.rs
@@ -8,11 +8,18 @@ pub struct ArrayBuffer<T: Sized> {
 impl<T: Sized> ArrayBuffer<T> {
     pub fn new(size: usize) -> Self {
         unsafe {
-            let layout = Layout::from_size_align_unchecked(size, std::mem::size_of::<T>());
+            let layout = Layout::from_size_align_unchecked(
+                size * std::mem::size_of::<T>(),
+                std::mem::size_of::<T>());
             let ptr = std::alloc::alloc(layout) as *mut T;
 
             Self { layout, ptr }
         }
+    }
+
+    #[inline]
+    pub fn size(&self) -> usize {
+        self.layout.size() / std::mem::size_of::<T>()
     }
 }
 
