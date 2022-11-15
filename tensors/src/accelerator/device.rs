319166ebf0e202df447b14bc9b2263ebc58e2d9b
neilcantorne <cantorne.neil@gmail.com>
2022-11-15 18:58:35 +0800

    impl fn name for OpenCL Device

--- tensors/src/accelerator/device.rs
+++ tensors/src/accelerator/device.rs
@@ -1,15 +1,20 @@
-use super::backend::OpenCl;
+use std::ffi::CStr;
+
+use super::backend::{OpenCl, ParamName};
 
 pub struct Device {
-    pub(crate) inner: Box<dyn DeviceInner>
+    pub(in crate::accelerator) inner: Box<dyn DeviceInner>
 }
 
 pub(crate) trait DeviceInner {
-
+    fn name(&self) -> crate::Result<String>;
 }
 
 impl Device {
-    
+    #[inline(always)]
+    pub fn name(&self) -> crate::Result<String> {
+        self.inner.name()
+    }
 }
 
 pub(crate) struct OpenClDevice {
@@ -18,5 +23,34 @@ pub(crate) struct OpenClDevice {
 }
 
 impl DeviceInner for OpenClDevice {
-    
+    fn name(&self) -> crate::Result<String> {
+        unsafe {
+            let mut size = 0usize;
+            
+            self.open_cl.get_device_info(
+                self.id,
+                ParamName::DeviceName,
+                0,
+                std::ptr::null_mut(),
+                &mut size)
+                .to_result()?;
+
+            let mut buffer = Vec::<i8>::with_capacity(size);
+            
+            self.open_cl.get_device_info(
+                self.id,
+                ParamName::DeviceName,
+                size,
+                buffer.as_mut_ptr() as *mut (),
+                std::ptr::null_mut())
+                .to_result()?;
+            
+            buffer.set_len(size);
+
+            Ok(CStr::from_ptr(buffer.as_mut_ptr())
+            .to_str()
+            .unwrap()
+            .to_owned())
+        }
+    }
 }
