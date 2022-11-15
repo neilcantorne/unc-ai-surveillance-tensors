7ae8832d574d08f898e6be73d169ae31592170cf
neilcantorne <cantorne.neil@gmail.com>
2022-11-15 20:41:56 +0800

    impl fn new for ArrayBuffer

--- tensors/src/accelerator/array_buffer.rs
+++ tensors/src/accelerator/array_buffer.rs
@@ -4,3 +4,14 @@ pub struct ArrayBuffer<T: Sized> {
     ptr: *mut T,
     layout: Layout
 }
+
+impl<T: Sized> ArrayBuffer<T> {
+    pub fn new(size: usize) -> Self {
+        unsafe {
+            let layout = Layout::from_size_align_unchecked(size, std::mem::size_of::<T>());
+            let ptr = std::alloc::alloc(layout) as *mut T;
+
+            Self { layout, ptr }
+        }
+    }
+}
