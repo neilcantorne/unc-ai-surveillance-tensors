8774ba5cb39e0631e493cd9fe100ad7e8790952f
neilcantorne <cantorne.neil@gmail.com>
2022-11-24 05:52:07 +0800

    derive Clone and Derive

--- tensors/src/accelerator/reflection/field_info.rs
+++ tensors/src/accelerator/reflection/field_info.rs
@@ -1,3 +1,5 @@
+
+#[derive(Debug, Clone)]
 pub struct FieldInfo {
     pub identifier: super::Identifier,
     pub date_type: super::DataType,
--- tensors/src/accelerator/reflection/identifier.rs
+++ tensors/src/accelerator/reflection/identifier.rs
@@ -1,3 +1,5 @@
+
+#[derive(Clone, Debug)]
 pub struct Identifier {
     value: String,
 }
