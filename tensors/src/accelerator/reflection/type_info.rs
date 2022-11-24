ae24207ac9e015b4cf7f158fe6722febd9e8ff53
neilcantorne <cantorne.neil@gmail.com>
2022-11-25 04:00:40 +0800

    implement AsKernelType for rust native types

--- tensors/src/accelerator/reflection/type_info.rs
+++ tensors/src/accelerator/reflection/type_info.rs
@@ -22,3 +22,52 @@ pub enum TypeInfo {
 pub trait AsKernelType {
     fn type_info() -> TypeInfo;
 }
+
+impl AsKernelType for i8 {
+    fn type_info() -> TypeInfo { TypeInfo::I8 }
+}
+
+impl AsKernelType for i16 {
+    fn type_info() -> TypeInfo { TypeInfo::I16 }
+}
+
+impl AsKernelType for i32 {
+    fn type_info() -> TypeInfo { TypeInfo::I32 }
+}
+
+impl AsKernelType for i64 {
+    fn type_info() -> TypeInfo { TypeInfo::I64 }
+}
+
+impl AsKernelType for u8 {
+    fn type_info() -> TypeInfo { TypeInfo::U8 }
+}
+
+impl AsKernelType for u16 {
+    fn type_info() -> TypeInfo { TypeInfo::U16 }
+}
+
+impl AsKernelType for u32 {
+    fn type_info() -> TypeInfo { TypeInfo::U32 }
+}
+
+impl AsKernelType for u64 {
+    fn type_info() -> TypeInfo { TypeInfo::U64 }
+}
+
+impl AsKernelType for f32 {
+    fn type_info() -> TypeInfo { TypeInfo::F32 }
+}
+
+impl AsKernelType for f64 {
+    fn type_info() -> TypeInfo { TypeInfo::F64 }
+}
+
+impl<T: AsKernelType, const SIZE: usize> AsKernelType for [T; SIZE] {
+    fn type_info() -> TypeInfo {
+        TypeInfo::Array {
+            element_type: Box::new(T::type_info()),
+            size: SIZE
+        }
+    }
+}
