33c7a33b2632037924e19cecc6a9b054867c38ca
neilcantorne <cantorne.neil@gmail.com>
2022-11-25 03:09:59 +0800

    impl From<String> for Identifier

--- tensors/src/accelerator/reflection/identifier.rs
+++ tensors/src/accelerator/reflection/identifier.rs
@@ -1,5 +1,17 @@
 
 #[derive(Clone, Debug)]
-pub struct Identifier {
-    value: String,
+pub struct Identifier(String);
+
+impl From<String> for Identifier {
+    #[inline]
+    fn from(value: String) -> Self {
+        Self(value)
+    }
+}
+
+impl From<&str> for Identifier {
+    #[inline]
+    fn from(value: &str) -> Self {
+        Self(value.to_owned())
+    }
 }
