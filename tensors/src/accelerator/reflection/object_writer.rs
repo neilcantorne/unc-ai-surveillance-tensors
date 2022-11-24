fd5412b4d7db20e24eb9e285cf9664dcf9c377e2
neilcantorne <cantorne.neil@gmail.com>
2022-11-25 05:15:08 +0800

    impl ObjectWriter

--- tensors/src/accelerator/reflection/object_writer.rs
+++ tensors/src/accelerator/reflection/object_writer.rs
@@ -1,7 +1,28 @@
 use std::ptr::NonNull;
-use std::alloc::Layout;
+use std::alloc::{
+    alloc,
+    Layout
+};
 
 pub struct ObjectWriter {
     ptr: NonNull<u8>,
     layout: Layout,
+}
+
+impl ObjectWriter {
+    pub(in crate::accelerator) fn new(size: usize, align: usize) -> Self {
+        unsafe {
+            let layout = Layout::from_size_align_unchecked(size, align);
+            Self {
+                layout,
+                ptr: NonNull::new(alloc(layout))
+                    .expect("ObjectBuffer memory allocation failed")
+            }
+        }
+    }
+
+    #[inline]
+    pub fn size(&self) -> usize {
+        self.layout.size()
+    }
 }
