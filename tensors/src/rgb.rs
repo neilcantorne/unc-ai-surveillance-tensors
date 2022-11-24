f0880a865020cf4e55b6a7d0606929beed726141
neilcantorne <cantorne.neil@gmail.com>
2022-11-25 04:51:08 +0800

    impl AsKernelStruct for Rgb

--- tensors/src/rgb.rs
+++ tensors/src/rgb.rs
@@ -1,3 +1,10 @@
+use crate::accelerator::reflection::{
+    AsKernelStruct,
+    AsKernelType,
+    StructInfo,
+    Identifier,
+    FieldInfo
+};
 
 pub struct Rgb<T>
     where T: Sized + Default {
@@ -10,5 +17,34 @@ impl<T> Default for Rgb<T>
     where T: Sized + Default {
     fn default() -> Self {
         Self { r: Default::default(), g: Default::default(), b: Default::default() }
+    }
+}
+
+impl<T> AsKernelStruct for Rgb<T>
+    where T: Sized + Default + AsKernelType {
+    fn struct_info() -> StructInfo {
+        StructInfo {
+            identifier: Identifier::from("Rgb"),
+            fields: {
+                let mut fields = Vec::with_capacity(3);
+                
+                fields.push(FieldInfo {
+                    identifier: Identifier::from("r"),
+                    date_type: <T as AsKernelType>::type_info()
+                });
+
+                fields.push(FieldInfo {
+                    identifier: Identifier::from("g"),
+                    date_type: <T as AsKernelType>::type_info()
+                });
+
+                fields.push(FieldInfo {
+                    identifier: Identifier::from("b"),
+                    date_type: <T as AsKernelType>::type_info()
+                });
+
+                fields
+            },
+        }
     }
 }
