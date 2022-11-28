a0a7aca2e4d6da33d35907028e73a0b18cdc15b1
neilcantorne <cantorne.neil@gmail.com>
2022-11-29 04:49:23 +0800

    add OpenContext

--- tensors/src/accelerator/context.rs
+++ tensors/src/accelerator/context.rs
@@ -1,7 +1,15 @@
+use super::backend::OpenCl;
+
 pub struct Context {
     pub(crate) inner: Box<dyn ContextInner>
 }
 
 pub(crate) trait ContextInner {
     
+}
+
+pub(crate) struct OpenContext {
+    pub open_cl: OpenCl,
+    pub context: usize,
+    pub device: usize,
 }
