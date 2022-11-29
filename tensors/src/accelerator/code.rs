485bf05e4ac37c6838485d6db140f924956c55ac
neilcantorne <cantorne.neil@gmail.com>
2022-11-30 06:09:37 +0800

    add OpenClCode

--- tensors/src/accelerator/code.rs
+++ tensors/src/accelerator/code.rs
@@ -6,3 +6,17 @@ pub(crate) trait CodeInner: Drop {
 
 }
 
+pub(crate) struct OpenClCode {
+    open_cl: super::backend::OpenCl,
+    program: usize,
+}
+
+impl CodeInner for OpenClCode {
+
+}
+
+impl Drop for OpenClCode {
+    fn drop(&mut self) {
+        unsafe { self.open_cl.release_program(self.program).to_result().unwrap() }
+    }
+}
