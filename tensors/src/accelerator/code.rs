b55fa26f702d2913d8a0a467851fe57c38f4640e
neilcantorne <cantorne.neil@gmail.com>
2022-12-01 08:21:05 +0800

    impl fn get_kernel

--- tensors/src/accelerator/code.rs
+++ tensors/src/accelerator/code.rs
@@ -1,9 +1,23 @@
+use std::ffi::CString;
+
 pub struct Code {
     pub(crate) inner: Box<dyn CodeInner>
 }
 
-pub(crate) trait CodeInner: Drop {
+impl Code {
+    #[inline]
+    pub fn get_kernel<P: super::KernelParameter>(&self, kernel_name: impl Into<Vec<u8>>) -> crate::Result<super::Kernel<P>> {
+        unsafe {
+            std::mem::transmute(self.inner.get_kernel(
+                CString::new(kernel_name)
+                    .ok()
+                    .ok_or(crate::Error::from("Failed conversion to C string"))?))
+        }
+    }
+}
 
+pub(crate) trait CodeInner: Drop {
+    fn get_kernel(&self, kernel_name: CString) -> crate::Result<super::Kernel<()>>;
 }
 
 pub(crate) struct OpenClCode {
@@ -12,7 +26,22 @@ pub(crate) struct OpenClCode {
 }
 
 impl CodeInner for OpenClCode {
+    #[allow(invalid_value)]
+    fn get_kernel(&self, kernel_name: CString) -> crate::Result<super::Kernel<()>> {
+        Ok(super::Kernel {
+                inner: Box::new(super::kernel::OpenClKernel {
+                    open_cl: self.open_cl.clone(),
+                    kernel: unsafe {
+                        let mut error = std::mem::MaybeUninit::uninit().assume_init();
+                        let kernel = self.open_cl.create_kernel(self.program, kernel_name.as_ptr(), &mut error);
 
+                        error.to_result()?;
+                        kernel
+                    }
+                })
+            }
+        )
+    }
 }
 
 impl Drop for OpenClCode {
