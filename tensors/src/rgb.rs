0be7fabf6d980d1d549cbbb82fd0899b845ce995
neilcantorne <cantorne.neil@gmail.com>
2022-12-14 13:34:27 +0800

    impl PushAsKernelArg for Rgb<T>

--- tensors/src/rgb.rs
+++ tensors/src/rgb.rs
@@ -1,3 +1,6 @@
+use crate::accelerator::{ PushAsKernelArg, KernelArgsStack };
+
+#[derive(Clone, Copy)]
 pub struct Rgb<T>
     where T: Sized + Default {
     pub r: T,
@@ -12,6 +15,10 @@ impl<T> Default for Rgb<T>
     }
 }
 
-impl<T: Sized + Default> crate::accelerator::AsKernelArg for Rgb<T> {
-
+impl<T: Sized + Default> PushAsKernelArg for Rgb<T> {
+    fn push(&self, stack: &mut KernelArgsStack<'_>) {
+        stack.push_c_buffer(
+            self as *const Self as *const (),
+            std::mem::size_of::<Self>())
+    }
 }
