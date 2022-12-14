e82f4d72520f7fa289e1f821a6c6bff0d5ac2c57
neilcantorne <cantorne.neil@gmail.com>
2022-12-14 13:37:49 +0800

    improve LayerBuilder

--- tensors/src/dnn/layer.rs
+++ tensors/src/dnn/layer.rs
@@ -1,4 +1,11 @@
-pub trait LayerBuilder {
-    fn build(&self, builder: &mut super::ModelBuilder)
-        -> crate::Result<()>;
+use crate::accelerator::PushAsKernelArg;
+
+pub trait LayerBuilder where Self::Input: PushAsKernelArg, Self::Output: PushAsKernelArg {
+    type Input;
+    type Output;
+
+    fn build<'a>(&self,
+        builder: &'a mut super::ModelBuilder,
+        input: super::LayerBuffer<'a, Self::Input>)
+        -> crate::Result<super::LayerBuffer<'a, Self::Output>>;
 }
